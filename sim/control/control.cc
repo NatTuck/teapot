168531d4e7e61bdc64fbdc3d659cec9629dec733
Nat Tuck <nat@ferrus.net>
2018-11-06 22:23:02 -0500

    Initial commit.

---
+++ sim/control/control.cc
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+
+namespace gazebo {
+    class ControlPlugin : public ModelPlugin
+    {
+    public:
+        ControlPlugin() {}
+        virtual void Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+        {
+            std::cerr << "hello from ControlPlugin" << std::endl;
+            std::cerr << "model: " << model->GetName() << std::endl;
+        }
+    };
+
+    GZ_REGISTER_MODEL_PLUGIN(ControlPlugin)
+}
+
