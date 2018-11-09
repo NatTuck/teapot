a506cfe048b9ceb8e80f3aeaa179ad9a72833004
Nat Tuck <Nat Tuck>
2018-11-09 12:31:16 -0500

    Directory structure; Add lidar model.

--- sim/sim.sh
+++ sim/sim.sh
@@ -2,7 +2,7 @@
 WD=`pwd`
 
 source /usr/share/gazebo/setup.sh
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$WD
-export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$WD/car_control
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$WD/models
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$WD/plugins
 
 gazebo --versbose demo.world
